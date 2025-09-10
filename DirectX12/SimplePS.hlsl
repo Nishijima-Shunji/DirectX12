c7d27c63ff147cb5baeaa77559b3d177a4dfc444
Nishijima-shunji <146911103+Nishijima-Shunji@users.noreply.github.com>
2025-09-10 13:55:06 +0900

    Revert "Fix invisible cube and fluid"
    This reverts commit 58588acd443c9163524185c66a29805893a845db.

--- DirectX12/SimplePS.hlsl
+++ DirectX12/SimplePS.hlsl
@@ -2,17 +2,13 @@ struct VSOutput
 {
     float4 svpos : SV_POSITION;
     float4 color : COLOR;
-    float2 uv    : TEXCOORD;
+    float2 uv : TEXCOORD;
 };
 
-SamplerState smp    : register(s0);
-Texture2D    _MainTex : register(t0);
+SamplerState smp : register(s0); // �T���v���[
+Texture2D _MainTex : register(t0); // �e�N�X�`��
 
 float4 pixel(VSOutput input) : SV_TARGET
 {
-    // テクスチャがバインドされていない場合は0が返るため、
-    // アルファ値を用いて頂点カラーへフォールバックする
-    float4 tex = _MainTex.Sample(smp, input.uv);
-    return lerp(input.color, tex, tex.a);
+    return _MainTex.Sample(smp, input.uv);
 }
-
